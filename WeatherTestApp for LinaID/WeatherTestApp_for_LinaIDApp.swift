0c6fb37142e5379317848cc718280847d0efaa84
Andrey Kaldyaev <2033012@mail.ru>
2026-02-11 15:32:36 +0300

    Initial Commit

---
+++ WeatherTestApp for LinaID/WeatherTestApp_for_LinaIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestApp_for_LinaIDApp.swift
+//  WeatherTestApp for LinaID
+//
+//  Created by Andrey Kaldyaev on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestApp_for_LinaIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
